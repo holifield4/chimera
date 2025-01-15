62e665f8919344c6a4f32bb253fbac54dfc78d01
holifieldw <holifield04@gmail.com>
2025-01-15 21:43:07 +0800

    feat: add navbar component

---
+++ src/app/components/navbar/NavBar.tsx
@@ -0,0 +1,10 @@
+import { pageRoutes } from "@/app/lib/constant/routes.constant";
+import NavBarButton from "./NavBarButton";
+
+export default function NavBar(){
+    return (
+        <nav className="w-full h-20 fixed z-50 shadow-[0_0_3px_0px] shadow-neutral bg-secondary flex items-center px-4 gap-4">
+            <NavBarButton routes={pageRoutes}/>
+        </nav>
+    )
+}
