e149c1fd9c66f787f9b816d24566c0a68bc5a5a3
holifieldw <holifield04@gmail.com>
2025-01-15 21:42:39 +0800

    feat: add routes constant and interface

---
+++ src/app/lib/constant/routes.constant.ts
@@ -0,0 +1,6 @@
+import { RouteInterface } from "../interfaces/route.interface";
+
+export const pageRoutes: RouteInterface[] = [
+    { key: "eqp-reservations", name: "EQP Reservations", url: '/'},
+    { key: "plc-control", name: "PLC Control", url: '/plccontrol'},
+]
---
+++ src/app/lib/interfaces/route.interface.ts
@@ -0,0 +1,5 @@
+export interface RouteInterface {
+    key: string;
+    name: string;
+    url: string;
+}
