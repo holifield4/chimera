9e22575a94aca19a4f483e49e4bf714b7e88335c
holifieldw <holifield04@gmail.com>
2025-01-16 17:03:33 +0800

    feat: change to server component and apply async

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,78 @@
-"use client";
-import { useEffect, useState } from "react";
 import DataTable, { Column } from "./components/table/DataTable";
+import { DateTimeHelper } from "./lib/helper/DateTimeHelper";
+import { IconCheck } from "./lib/icon/Check";
+import { XMark } from "./lib/icon/XMark";
 import { EqpReservationInterface } from "./lib/interfaces/eqp.interface";
 
-export default function Home() {
-  const [data, setData] = useState<EqpReservationInterface[]>([]);
-
-  useEffect(() => {
-    async function getData() {
-      await fetch("/api/EqpReservations")
-        .then(async (data) => {
-          let response = await data.json();
-          setData(await response.data);
-        })
-        .catch((error) => console.error("Error fetching data:", error));
-    }
-    getData();
-    console.log(data);
-  }, []);
+export default async function Home() {
+  const data: EqpReservationInterface[] = await fetch(
+    "http://localhost:3000/api/eqpreservations"
+  )
+    .then(async (data) => {
+      let response = await data.json();
+      return response.data;
+    })
+    .catch((error) => console.error("Error fetching data:", error));
 
   const columns: Column<EqpReservationInterface>[] = [
     { key: "BookSNo", header: "Book's No." },
     { key: "RecordSource", header: "Record Source" },
     { key: "Equipment", header: "Equipment" },
-    { key: "ActState", header: "Active State" },
-    { key: "ActTime", header: "Active Time" },
-    { key: "DeactTime", header: "Deactive Time" },
-    { key: "CardEnable", header: "Card Enable" },
-    { key: "Init", header: "Init" },
+    {
+      key: "ActState",
+      header: "Active State",
+      render: (value) =>
+        value ? (
+          <IconCheck className="w-full text-accent" />
+        ) : (
+          <XMark className="w-full text-red-600" />
+        ),
+    },
+    {
+      key: "ActTime",
+      header: "Active Time",
+      render: (value) =>
+        value ? (
+          <IconCheck className="w-full text-accent" />
+        ) : (
+          <XMark className="w-full text-red-600" />
+        ),
+    },
+    {
+      key: "DeactTime",
+      header: "Deactive Time",
+      render: (value) => (
+        <span>
+          {String(DateTimeHelper(value))}
+        </span>
+      ),
+    },
+    {
+      key: "CardEnable",
+      header: "Card Enable",
+      render: (value) =>
+        value ? (
+          <IconCheck className="w-full text-accent" />
+        ) : (
+          <XMark className="w-full text-red-600" />
+        ),
+    },
+    {
+      key: "Init",
+      header: "Init",
+      render: (value) =>
+        value ? (
+          <IconCheck className="w-full text-accent" />
+        ) : (
+          <XMark className="w-full text-red-600" />
+        ),
+    },
   ];
 
   return (
-    <div className="flex-grow pt-[6.5rem] pb-4 overflow-hidden">
+    <div className="flex-grow pt-[6.5rem] pb-8 overflow-hidden">
       <main className="h-full w-full px-4 overflow-hidden">
-        {/* TODO: add unique keys */}
-        <DataTable columns={columns} data={data} />
+        <DataTable<EqpReservationInterface> columns={columns} data={data} />
       </main>
     </div>
   );
