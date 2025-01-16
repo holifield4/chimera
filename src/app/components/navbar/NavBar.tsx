7f1ce16ebbdecbbf0047c8d3ad87d596e7de0df8
holifieldw <holifield04@gmail.com>
2025-01-16 17:02:01 +0800

    style: modify styling

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -3,7 +3,7 @@ import NavBarButton from "./NavBarButton";
 
 export default function NavBar(){
     return (
-        <nav className="w-full h-20 fixed z-50 shadow-[0_0_3px_0px] shadow-neutral bg-secondary flex items-center px-4 gap-4">
+        <nav className="w-full h-20 fixed z-50 shadow-[0_0_3px_0] shadow-neutral bg-secondary flex items-center px-4 gap-4">
             <NavBarButton routes={pageRoutes}/>
         </nav>
     )
